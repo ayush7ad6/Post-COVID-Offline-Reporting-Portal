c8402a7172fdaf3ca9ad43048d9f37d2918d185c
Ayush Dubey <2019221@iiitdmj.ac.in>
2021-08-25 19:35:11 +0530

    fixed: credential leak

--- date.php
+++ date.php
@@ -17,8 +17,8 @@ try {
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'thenarci001@gmail.com';                     // SMTP username
-    $mail->Password   = 'probe1010';                               // SMTP password
+    $mail->Username   = 'youremailid';                     // SMTP username
+    $mail->Password   = 'yourpassword';                               // SMTP password
     $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
